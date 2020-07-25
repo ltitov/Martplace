3c1e8645e3990f83cbb06970940097dd1ff53b76
Ltitov <itsitou88@gmail.com>
2020-07-25 12:06:52 +0300

    added featured -product  section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function () {
 
+  $(function () {
+ 
+    $(".rate-star").rateYo({
+      rating: 4.5,
+      readOnly: true,
+      starWidth: "14px",
+      spacing: "4px"
+    });
+   
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
   var mixer = mixitup('.products__inner-box');
   
   
